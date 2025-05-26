ad9b257718ac7710b507e37e0435175840842430
Putri Widianingsih <n.putriwidia21@gmail.com>
2025-05-26 22:56:41 +0700

    update

--- resources/views/inventaris-admin.blade.php
+++ resources/views/inventaris-admin.blade.php
@@ -5,7 +5,7 @@
     <title>Inventaris Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/invetaris-admin.css') }}">
-    <link rel="stylesheet" href="https://liliytoys-tubes-production-123e.up.railway.app/css/inventaris-admin.css">
+    <link rel="stylesheet" href="https://liliytoys-tubes-production-123e.up.railway.app/css/invetaris-admin.css">
 
 </head>
 <body>
